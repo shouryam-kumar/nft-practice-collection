057928d01f0949c2d833d3e0c054fa8cbbc58466
Shouryam Kumar <shouryam1508@gmail.com>
2022-04-17 20:43:15 +0530

    bug fix

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -10,7 +10,7 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
 
-const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const CONTRACT_ADDRESS = "0xc0D3139a53b31abB19DABDc5D29c473C9D770D21";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
